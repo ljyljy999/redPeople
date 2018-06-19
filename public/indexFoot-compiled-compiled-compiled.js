847f65b10641b2e4be9d8bcbdd04044c51ac4e51
“13834156253li” <478518129@qq.com>
2018-06-19 14:08:18 +0800

    李静妍  6月19   上传 index news newscon  discovery 四个页面

---
+++ public/indexFoot-compiled-compiled-compiled.js
@@ -0,0 +1,32 @@
+"use strict";
+
+document.writeln("<footer>");
+document.writeln("			<nav class=\'mui-bar mui-bar-tab\'>");
+document.writeln("				<a class=\'mui-tab-item mui-active\'>");
+document.writeln("					<span class=\'mui-icon icon1\'></span>");
+document.writeln("				    <span class=\'mui-tab-label\'>首页</span>");
+document.writeln("				</a>");
+document.writeln("				<a class=\'mui-tab-item\'>");
+document.writeln("				    <span class=\'mui-icon icon2\'></span>");
+document.writeln("				    <span class=\'mui-tab-label\'>学堂</span>");
+document.writeln("				</a>");
+document.writeln("				<a class=\'mui-tab-item\'>");
+document.writeln("				    <span class=\'mui-icon icon3\'></span>");
+document.writeln("				    <span class=\'mui-tab-label\'>注册</span>");
+document.writeln("				</a>");
+document.writeln("				<a class=\'mui-tab-item\'>");
+document.writeln("				    <span class=\'mui-icon icon4\'></span>");
+document.writeln("				    <span class=\'mui-tab-label\'>问答</span>");
+document.writeln("				    </a>");
+document.writeln("				<a class=\'mui-tab-item\'>");
+document.writeln("				    <span class=\'mui-icon icon5\'></span>");
+document.writeln("				    <span class=\'mui-tab-label\'>我的</span>");
+document.writeln("				</a>");
+document.writeln("			</nav>");
+document.writeln("		</footer>	");
+
+//# sourceMappingURL=indexFoot-compiled.js.map
+
+//# sourceMappingURL=indexFoot-compiled-compiled.js.map
+
+//# sourceMappingURL=indexFoot-compiled-compiled-compiled.js.map
---
+++ public/indexSwiper-compiled.js
@@ -0,0 +1,28 @@
+"use strict";
+
+document.writeln("<div class=\'mui-scroll\'>");
+document.writeln("			        <div class=\'mui-slider\' >");
+document.writeln("					  <div class=\'mui-slider-group mui-slider-loop\'>");
+document.writeln("					    <!-- 额外增加的一个节点(循环轮播：第一个节点是最后一张轮播) -->");
+document.writeln("					    <div class=\'mui-slider-item mui-slider-item-duplicate\'>");
+document.writeln("					      <a href=\'#\'>");
+document.writeln("					        <img src=\'images/banner.jpg\'>");
+document.writeln("					      </a>");
+document.writeln("					    </div>");
+document.writeln("					    <!-- 第一张 -->");
+document.writeln("					    <div class=\'mui-slider-item\'>");
+document.writeln("					      <a href=\'#\'>");
+document.writeln("					         <img src=\'images/banner.jpg\'>");
+document.writeln("					      </a>");
+document.writeln("					    </div>");
+document.writeln("					    <!-- 额外增加的一个节点(循环轮播：最后一个节点是第一张轮播) -->");
+document.writeln("					    <div class=\'mui-slider-item mui-slider-item-duplicate\'>");
+document.writeln("					      <a href=\'#\'>");
+document.writeln("					         <img src=\'images/banner.jpg\'>");
+document.writeln("					      </a>");
+document.writeln("					    </div>");
+document.writeln("					  </div>");
+document.writeln("					</div>");
+document.writeln("			      </div>");
+
+//# sourceMappingURL=indexSwiper-compiled.js.map
